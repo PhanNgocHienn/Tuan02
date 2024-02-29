f51cee8a28833053d575c0db630b08e053261008
Phan Ngoc Hien <phanngochien0101@gmail.com>
2024-02-29 15:42:57 +0700

    first commit

---
+++ Student.java
@@ -0,0 +1,17 @@
+public class student {
+    //props
+    private String id;
+    private String name;
+
+    private String add;
+
+//methods
+
+    public String getName() {
+        return name;
+    }
+
+    public String detId() {
+        return id;
+    }
+}
